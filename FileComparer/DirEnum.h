b07df3dde8a3f387006c16dfea91bc0e9e829212
elGeminy <elGeminy@gmail.com>
2018-12-13 16:09:43 +0200

    moving to std::regex

--- FileComparer/DirEnum.h
+++ FileComparer/DirEnum.h
@@ -3,8 +3,6 @@
 #include "File.h"
 
 using ListOfStrings = std::list <std::wstring>;
-using VectorOfStrings = std::vector <std::wstring>;
-using ListOfVecsOfStrings = std::list <VectorOfStrings>;
 using ListOfPaths = std::list <std::filesystem::path>;
 
 struct EnumException
@@ -14,23 +12,26 @@ struct EnumException
 
 class DirEnumerator
 {
+private:
 	ListOfPaths m_dir_pathes;
 	ListOfPaths m_file_pathes;
 
 	uintmax_t m_min_size = 0;
 	uintmax_t m_max_size = (uintmax_t)-1;
 
-	ListOfVecsOfStrings m_exc_file_mask;
-	ListOfVecsOfStrings m_exc_dir_mask;
+	ListOfStrings m_exc_file_mask;
+	ListOfStrings m_exc_dir_mask;
 
-	ListOfVecsOfStrings m_inc_file_mask;
-	ListOfVecsOfStrings m_inc_dir_mask;
+	ListOfStrings m_inc_file_mask;
+	ListOfStrings m_inc_dir_mask;
 
 	bool m_opt_file = false;
 	bool m_opt_dir = false;
 
-	bool MatchMask (const VectorOfStrings & mask, const std::wstring & str);
-	void AddFileList (ListOfStrings & list, ListOfVecsOfStrings & add_to);
+private:
+	bool MatchMask (const std::wstring & mask, const std::wstring & str);
+
+	void AddFileList (ListOfStrings & list, ListOfStrings & add_to);
 	bool IsObjectIgnored (const wchar_t * pobj, bool is_dir, uintmax_t filesize);
 	void EnumerateDirectory (const std::filesystem::path & root, ListOfStrings & ignored, std::map <uintmax_t, ListOfFiles> & found);
 	void EnumerateDirectory_Win7 (const std::filesystem::path & root, ListOfStrings & ignored, std::map <uintmax_t, ListOfFiles> & files);
